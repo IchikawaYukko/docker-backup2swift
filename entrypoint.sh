bb05e035c05bede79c155d31f921a75bca6a9bd8
IchikawaYukko <ichikawayuriko@yahoo.co.jp>
2018-08-31 00:14:58 +0900

    add -q option to swift command. because of mail from cron is annoy.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ source /OpenStackAuth.sh
 source /opt/rh/rh-php70/enable
 
 tar cfpj /temp.tar.bz2 $DATAVOLUMES --warning=no-file-changed
-swift upload --object-name $BACKUP_FILENAME -S 5368709119 $BACKUP_CONTAINER /temp.tar.bz2
+swift -q upload --object-name $BACKUP_FILENAME -S 5368709119 $BACKUP_CONTAINER /temp.tar.bz2
 
 # set Auto delete
 swift post -H "X-Delete-After: $(php /hanoi-tower-backup.php)" $BACKUP_CONTAINER $BACKUP_FILENAME
